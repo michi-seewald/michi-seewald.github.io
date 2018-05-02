a694ae474b0ffd5039264b00b11bf2e5069dab66
Michaela Seewald <michaela.seewald@student.uibk.ac.at>
2018-05-02 17:22:33 +0200

    aufgabe stadtspaziergang wien

---
+++ wien.js
@@ -0,0 +1,87 @@
+let myMap = L.map("mapdiv"); // http://leafletjs.com/reference-1.3.0.html#map-l-map
+const awsGroup = L.featureGroup();
+let myLayers = {
+    osm : L.tileLayer( // http://leafletjs.com/reference-1.3.0.html#tilelayer
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"], // subdomains hinzugefuegt https://gis.stackexchange.com/questions/201881/what-is-an-appropriate-tilelayer-source-for-tiles-served-on-an-apache-localhost
+        attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>OpenStreetMap"
+        }
+    ),
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" // http://leafletjs.com/reference-1.3.0.html#layer-attribution
+    }
+    ),
+    bmapoverlay :  L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmapgrau : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaphidpi : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaporthofoto30cm : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    basemapneu : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" // http://leafletjs.com/reference-1.3.0.html#layer-attribution
+    }
+    ),
+}
+
+myMap.addLayer(myLayers.geolandbasemap); // http://leafletjs.com/reference-1.3.0.html#map-addlayer
+
+let myMapControl = L.control.layers({ // http://leafletjs.com/reference-1.3.0.html#control-layers
+    "OpenStreetMap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,
+   
+    "basemap.at grau" : myLayers.bmapgrau,
+    "basemap.at highdpi" : myLayers.bmaphidpi,
+    "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
+},{
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+    "Wetterstationen": awsGroup,
+},{
+    collapsed: false,
+});
+
+myMap.addControl(myMapControl); // http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+
+L.control.scale({ // http://leafletjs.com/reference-1.3.0.html#control-scale
+    position: 'bottomleft', // http://leafletjs.com/reference-1.3.0.html#control-scale
+    metric: true, // http://leafletjs.com/reference-1.3.0.html#control-scale
+    imperial: false, // http://leafletjs.com/reference-1.3.0.html#control-scale-imperial
+    maxWidth: 200,    // http://leafletjs.com/reference-1.3.0.html#control-scale
+}).addTo(myMap);
+
+
+myMap.setView([47.267,11.383],8); // http://leafletjs.com/reference-1.3.0.html#map-setview
+
+console.log("Stationen: ", stationen);
+
+myMap.addLayer(awsGroup);
+let geojson = L.geoJSON(stationen).addTo(awsGroup);
+geojson.bindPopup(function(layer) {
+    const props = layer.feature.properties;
+    const popupText = `<h1>${props.name}</h1>
+    <p>Temperatur: ${props.LT} °C</p>`;
+    return popupText;
+});
+
+myMap.fitBounds(awsGroup.getBounds());
