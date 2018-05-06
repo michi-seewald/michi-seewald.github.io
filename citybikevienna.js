20ded67ddf7b3b183592e708a03ffe9b3da1e3cb
Michaela Seewald <michaela.seewald@student.uibk.ac.at>
2018-05-06 20:27:47 +0200

    wl7 citybike

---
+++ citybikevienna.js
@@ -0,0 +1,84 @@
+let myMap = L.map("mapdiv");
+const bikeGroup = L.featureGroup();
+let myLayers = {
+    osm : L.tileLayer( 
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"], 
+        attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>OpenStreetMap"
+        }
+    ),
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], 
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" 
+    }
+    ),
+    bmapoverlay :  L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmapgrau : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaphidpi : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    bmaporthofoto30cm : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at"
+    }
+    ),
+    basemapneu : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], 
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" 
+    }
+    ),
+}
+
+myMap.addLayer(myLayers.geolandbasemap); 
+
+let myMapControl = L.control.layers({ 
+    "OpenStreetMap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,
+   
+    "basemap.at grau" : myLayers.bmapgrau,
+    "basemap.at highdpi" : myLayers.bmaphidpi,
+    "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
+},{
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+    "Citybike Wien": bikeGroup,
+},{
+    collapsed: false,
+});
+
+myMap.addControl(myMapControl); 
+
+L.control.scale({ 
+    position: 'bottomleft', 
+    metric: true, 
+    imperial: false, 
+    maxWidth: 200, 
+}).addTo(myMap);
+
+
+myMap.setView([48.226653,16.378609],8);
+
+let geojson = L.geoJSON(bikepoints).addTo(bikeGroup);
+geojson.bindPopup(function(layer) {
+    const props = layer.feature.properties;
+    const popupText = `<h1>${props.STATION}</h1>`;
+    return popupText;
+});
+
+myMap.addLayer(bikeGroup);
+myMap.fitBounds(bikeGroup.getBounds());
